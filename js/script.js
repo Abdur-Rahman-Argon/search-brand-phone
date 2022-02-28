508d6f065ad2f764463a897a840db40983728228
abdurrahman79 <abdurrahmanargon7@gmail.com>
2022-03-01 00:23:22 +0600

    detail added

--- js/script.js
+++ js/script.js
@@ -35,47 +35,59 @@ const detailLoad = (id) => {
     fetch(url)
         .then((res) => res.json())
         .then((data) => displayDetail(data));
-    // console.log(url);
+    // console.log(data);
 };
 
 const displayDetail = (detail) => {
-    console.log(detail.data);
+    console.log(detail.data.others.Bluetooth);
+    const element = detail.data;
+    const displayDetail = document.getElementById("display-detail");
     const div = document.createElement("div");
     div.className = "col";
     div.innerHTML = `
-    <div class="card p-3 text-center" style="width: 18rem">
-      <img  src=" ${detail.image}" class="card-img-top image-width" alt="..." />
+    <div class="card p-3 mx-auto card-width">
+      <img  src=" ${element.image}" class="card-img-top image-width-2" alt="..." />
       <div class="card-body">
-      <h5 class="card-title">${detail.phone_name}</h5>
-      <h5 class="card-title">${detail.brand}</h5>
+      <h5 class="textcenter">${element.name}</h5>
+      <h5 class="textcenter">${element.brand}</h5>
+      <h5 class="textcenter">${element.releaseDate}</h5>
+      <h5 class="textcenter">${element.others.Bluetooth}</h5>
+      <h5 class="textcenter">${element.others.GPS}</h5>
+      <h5 class="textcenter">${element.others.NFC}</h5>
+      <h5 class="textcenter">${element.others.Radio}</h5>
+      <h5 class="textcenter">${element.others.USB}</h5>
+      <h5 class="textcenter">${element.others.WLAN}</h5>
+                    <ul>
+                        <li>${element.mainFeatures.chipSet}</li>
+                        <li>displaySize:${element.mainFeatures.displaySize}</li>
+                        <li>${element.mainFeatures.memory}</li>
+                        <li>sensors:
+                            <ul>
+                                <li>${element.mainFeatures.sensors[0]}</li>
+                                <li>${element.mainFeatures.sensors[1]}</li>
+                                <li>${element.mainFeatures.sensors[2]}</li>
+                                <li>${element.mainFeatures.sensors[3]}</li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+
       </div>
     </div>
         `;
+    displayDetail.appendChild(div);
 };
-
-/* 
-data: brand: "Apple"
-image: "https://fdn2.gsmarena.com/vv/bigpic/apple-iphone-13-pro.jpg"
-mainFeatures: chipSet: "Apple A15 Bionic (5 nm)"
-displaySize: "6.1 inches, 90.2 cm2 (~86.0% screen-to-body ratio)"
-memory: "128GB 6GB RAM, 256GB 6GB RAM, 512GB 6GB RAM, 1TB 6GB RAM"
-sensors: Array(6) 0: "Face ID"
-1: "accelerometer"
-2: "gyro"
-3: "proximity"
-4: "compass"
-5: "barometer"
-length: 6[[Prototype]]: Array(0) storage: "128GB/256GB/1TB storage, no card slot" [
-    [Prototype]
-]: Objectname: "iPhone 13 Pro"
-others: Bluetooth: "5.0, A2DP, LE"
-GPS: "Yes, with A-GPS, GLONASS, GALILEO, BDS, QZSS"
-NFC: "Yes"
-Radio: "No"
-USB: "Lightning, USB 2.0"
-WLAN: "Wi-Fi 802.11 a/b/g/n/ac/6, dual-band, hotspot" [
-    [Prototype]
-]: Objectconstructor: ƒ Object() hasOwnProperty: ƒ hasOwnProperty() isPrototypeOf: ƒ isPrototypeOf() propertyIsEnumerable: ƒ propertyIsEnumerable() toLocaleString: ƒ toLocaleString() toString: ƒ toString() valueOf: ƒ valueOf() __defineGetter__: ƒ __defineGetter__() __defineSetter__: ƒ __defineSetter__() __lookupGetter__: ƒ __lookupGetter__() __lookupSetter__: ƒ __lookupSetter__() __proto__: (...) get __proto__: ƒ __proto__() set __proto__: ƒ __proto__() releaseDate: ""
-slug: "apple_iphone_13_pro-11102" [
-    [Prototype]
-]: Objectstatus: true[[Prototype]]: Object */
+/* <h5 class="textcenter">${element.others.Bluetooth}</h5>
+      <h5 class="textcenter">${element.others.GPS}</h5>
+      <h5 class="textcenter">${element.others..NFC}</h5>
+      <h5 class="textcenter">${element.others.Radio}</h5>
+      <h5 class="textcenter">${element.others.USB}</h5>
+      <h5 class="textcenter">${element.others.WLAN}</h5> */
+// thers:
+// : "5.0, A2DP, LE"
+// GPS: "Yes, with A-GPS, GLONASS, GALILEO, QZSS"
+// NFC: "Yes"
+// Radio: "No"
+// USB: "No"
+// WLAN: "Wi-Fi 802.11 b/g/n"
+// [[
