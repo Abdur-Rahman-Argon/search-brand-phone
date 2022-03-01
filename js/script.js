2a2f4b9d95ea81e26b11c209b1e0932d1e01e3a4
abdurrahman79 <abdurrahmanargon7@gmail.com>
2022-03-01 08:56:07 +0600

    Error Handaling Add

--- js/script.js
+++ js/script.js
@@ -1,30 +1,44 @@
+const displaySearch = document.getElementById("display-search");
+const searchInput = document.getElementById("search-value");
+const searchError = document.getElementById("search-error");
 const searchLoad = async() => {
-    const searchInput = document.getElementById("search-value");
     const searchText = searchInput.value;
-    const url = ` https://openapi.programming-hero.com/api/phones?search=${searchText}`;
+    searchInput.value = "";
 
-    const res = await fetch(url);
-    const data = await res.json();
-    displayData(data.data);
+    if (searchText == "") {
+        searchError.innerText = "Please give a phone name";
+        displaySearch.textContent = "";
+    } else {
+        searchError.innerText = "";
+        const url = ` https://openapi.programming-hero.com/api/phones?search=${searchText}`;
+        const res = await fetch(url);
+        const data = await res.json();
+        if (data.status === false) {
+            searchError.innerText = "NO DATA FOUND";
+            displaySearch.textContent = "";
+        }
+        if (data.status === true) {
+            displayData(data.data);
+        }
+        console.log(data.status);
+    }
 };
 
 const displayData = (datas) => {
-    const displaySearch = document.getElementById("display-search");
+    displaySearch.textContent = "";
     datas.forEach((data) => {
-        // console.log(data);
         const div = document.createElement("div");
         div.className = "col";
         div.innerHTML = `
-    <div class="card p-3 text-center" style="width: 18rem">
-      <img  src=" ${data.image}" class="card-img-top image-width" alt="..." />
-      <div class="card-body">
-      <h5 class="card-title">${data.phone_name}</h5>
-      <h5 class="card-title">${data.brand}</h5>
-        <button onclick="detailLoad('${data.slug}')" href="#" class="btn btn-primary">
-          See Detail
-        </button>
-      </div>
-    </div>
+            <div class="card p-3 text-center" style="width: 18rem">
+                 <img  src=" ${data.image}" class="card-img-top image-width" alt="..." />
+                <div class="card-body">
+                    <h5 class="card-title">${data.phone_name}</h5>
+                    <h5 class="card-title">${data.brand}</h5>
+                    <button onclick="detailLoad('${data.slug}')" href="#"
+                    class="btn btn-primary">See Detail</button>
+                </div>
+            </div>
         `;
         displaySearch.appendChild(div);
     });
@@ -48,30 +62,28 @@ const displayDetail = (detail) => {
     <div class="card p-3 mx-auto card-width">
       <img  src=" ${element.image}" class="card-img-top image-width-2" alt="..." />
       <div class="card-body">
-      <h5 class="textcenter">${element.name}</h5>
-      <h5 class="textcenter">${element.brand}</h5>
-      <h5 class="textcenter">${element.releaseDate}</h5>
-      <h5 class="textcenter">${element.others.Bluetooth}</h5>
-      <h5 class="textcenter">${element.others.GPS}</h5>
-      <h5 class="textcenter">${element.others.NFC}</h5>
-      <h5 class="textcenter">${element.others.Radio}</h5>
-      <h5 class="textcenter">${element.others.USB}</h5>
-      <h5 class="textcenter">${element.others.WLAN}</h5>
-                    <ul>
-                        <li>${element.mainFeatures.chipSet}</li>
-                        <li>displaySize:${element.mainFeatures.displaySize}</li>
-                        <li>${element.mainFeatures.memory}</li>
-                        <li>sensors:
-                            <ul>
-                                <li>${element.mainFeatures.sensors[0]}</li>
-                                <li>${element.mainFeatures.sensors[1]}</li>
-                                <li>${element.mainFeatures.sensors[2]}</li>
-                                <li>${element.mainFeatures.sensors[3]}</li>
-                            </ul>
-                        </li>
-                    </ul>
-                </div>
-
+      <h5 class="textcenter"> ${element.name}</h5>
+      <h5 class="textcenter"> ${element.brand}</h5>
+      <h5 class="textcenter"> ${element.releaseDate}</h5>
+      <ul>
+      <li>chipSet: ${element.mainFeatures.chipSet}</li>
+      <li>displaySize: ${element.mainFeatures.displaySize}</li>
+      <li>memory: ${element.mainFeatures.memory}</li>
+      <li>sensors:
+      <ul>
+      <li>${element.mainFeatures.sensors[0]}</li>
+      <li>${element.mainFeatures.sensors[1]}</li>
+      <li>${element.mainFeatures.sensors[2]}</li>
+      <li>${element.mainFeatures.sensors[3]}</li>
+      </ul>
+      </li>
+      </ul> 
+      <h5 >Bluetooth: ${element.others.Bluetooth}</h5>
+      <h5 >GPS: ${element.others.GPS}</h5>
+      <h5 >NFC: ${element.others.NFC}</h5>
+      <h5 >Radio: ${element.others.Radio}</h5>
+      <h5 >USB: ${element.others.USB}</h5>
+      <h5 >WLAN: ${element.others.WLAN}</h5>
       </div>
     </div>
         `;
